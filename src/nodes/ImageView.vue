0f94d4f9e559d03fde0b04fb598e8dc984ecc98c
Michal Junek <michal@junek.pw>
2022-02-04 13:38:02 +0100

    [stable23] Prevents race condition when loading images

--- src/nodes/ImageView.vue
+++ src/nodes/ImageView.vue
@@ -172,7 +172,6 @@ export default {
 			return
 		}
 		const img = new Image()
-		img.src = this.imageUrl
 		img.onload = () => {
 			this.imageLoaded = true
 		}
@@ -181,6 +180,7 @@ export default {
 			this.imageLoaded = false
 			this.loaded = true
 		}
+		img.src = this.imageUrl
 	},
 	methods: {
 		updateAlt() {
